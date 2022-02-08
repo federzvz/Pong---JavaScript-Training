6f085d7a28222a18588177cdbf8aa9ada975048e
federzvz <85709865+federzvz@users.noreply.github.com>
2022-02-08 10:31:44 -0300

    Initial board structure created

---
+++ main.js
@@ -0,0 +1,39 @@
+(function () {
+    self.Board = function (width, height) {
+        this.width = width;
+        this.height = height;
+        this.playing = false;
+        this.game_over = false;
+        this.bars = [];
+        this.ball = null;
+        this.playing = false;
+    }
+
+    self.Board.prototype = { // metodo que retorna las barras y las pelotas que estan dentro del tablero
+        get elements() {
+            var elements = this.bars.map(function (bar) {
+                return bar;
+            });
+            elements.push(this.ball);
+            return elements;
+        }
+    }
+})();
+
+(function () {
+    self.BoardView = function (canvas, board) {
+        this.canvas = canvas;
+        this.canvas.width = board.width;
+        this.canvas.height = board.height;
+        this.board = board;
+        this.cxt = canvas.getContext("2d");
+    }
+})();
+
+self.addEventListener("load",main);
+
+function main(){
+    var borad = new Board(800,400);
+    var canvas = document.getElementById("canvas");
+    var board_view = new BoardView(canvas,board);
+}
